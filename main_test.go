e15c0db1d6772e712deac49f7db851da3ebd2251
Norbert Metz <mr.norbert.metz@googlemail.com>
2019-02-14 23:11:52 +0000

    Test all sides unique

--- main_test.go
+++ main_test.go
@@ -33,3 +33,56 @@ func TestFmtColor(t *testing.T) {
 		})
 	}
 }
+
+var allSidesUniqueTests = []struct {
+	in  dice
+	out bool
+}{
+	{
+		in: dice{
+			{green, green, green, green, green, green},
+			{blue, blue, blue, blue, blue},
+			{red, red, red, red, red, red},
+			{yellow, yellow, yellow, yellow, yellow, yellow},
+		},
+		out: true,
+	},
+	{
+		in: dice{
+			{green, yellow, red, blue, green, green},
+			{blue, green, yellow, red, blue},
+			{red, blue, green, yellow, red, red},
+			{yellow, red, blue, green, yellow, yellow},
+		},
+		out: true,
+	},
+	{
+		in: dice{
+			{green, green, yellow, green, green, green},
+			{blue, blue, blue, blue, blue},
+			{red, red, red, red, red, red},
+			{yellow, yellow, yellow, yellow, yellow, yellow},
+		},
+		out: false,
+	},
+	{
+		in: dice{
+			{green, yellow, red, blue, green, green},
+			{blue, blue, yellow, red, blue},
+			{red, blue, green, yellow, red, red},
+			{yellow, red, blue, green, yellow, yellow},
+		},
+		out: false,
+	},
+}
+
+func TestAllSidesUnique(t *testing.T) {
+	for _, tc := range allSidesUniqueTests {
+		t.Run("All sides unique", func(t *testing.T) {
+			res := tc.in.allSidesUnique()
+			if res != tc.out {
+				t.Errorf("\ngot  %v\nwant %v", res, tc.out)
+			}
+		})
+	}
+}
